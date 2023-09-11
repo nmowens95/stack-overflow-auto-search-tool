324172a5a661cf7129003f55411d637ca0910415
nmowens95 <nmowens95@gmail.com>
2023-09-11 16:39:58 -0500

    Was able to get it so that links would appear from command line with the error in mind, lost that when trying to translate over to opening windows. Was able to comment up a lot of code and make mostly functional

--- error_checker.py
+++ error_checker.py
@@ -1,17 +1,45 @@
 import subprocess
 
 # Create a wrapper script
-def python_code(user_code):
-    process = subprocess.Popen(
-        ["python", "-c", user_code],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        text=True # make sure output is in text format
-    )
-    stdout, stderr = process.communicate()
-    if process.returncode != 0:
+def execute_python_code(user_code):
+    try:  
+        process = subprocess.Popen(
+            ["python", "-c", user_code],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True # make sure output is in text format
+        )
+        stdout, stderr = process.communicate()
+
+        if process.returncode != 0:
+            return {
+                "success": False,
+                "error_type": "RunetimeError",
+                "error_message": stderr.strip()
+            }
+        else:
+            return {
+                "success": True,
+                "error_message": stderr.strip()
+            }
+    except Exception as err:
         return {
             "success": False,
-            "error_type": "RunetimeError",
-            "error_message": stderr.strip()
+            "error_type": "Exception",
+            "error_message": str(err)
         }
+    
+if __name__ == "__main__":
+    user_code = input(f"Enter your Python code: \n")
+    result = execute_python_code(user_code)
+
+    if result["success"]:
+        print("No errors")
+        print(f"Output: {user_code}")
+    else:
+        print("Error Type", result["error_type"])
+        print("Error Message", result["error_message"])
+
+    if not result["success"]:
+        import stack_overflow_api
+        stack_overflow_api.search_stackoverflow(result["error_message"], result["error_type"])
--- stack_overflow_api.py
+++ stack_overflow_api.py
@@ -1,5 +1,37 @@
 import requests
-import json
+import time
+import webbrowser
 
-response = requests.get("https://api.stackexchange.com")
-print(response)
+def search_stackoverflow(error_message, error_type, max_tabs=3):
+    # Stack exchange API base url
+    api_url = "https://api.stackexchange.com/"
+
+
+    try:
+        # Make an API request using the relevant parameters
+        response = requests.get(f"{api_url}"+f"/2.3/search?order=desc&tagged=python&sort=activity&site=stackoverflow&q={error_message}".format(error_type))
+        response.raise_for_status() # returns HTTPError object if an error occurs during the process
+
+        # Parses data
+        data = response.json()
+
+        # Get th list of relevant threads
+        relevant_threads = data["items"]
+
+        # Determine how many tabs to open
+        num_tabs_to_open = min(max_tabs, len(relevant_threads))
+
+        # Open relevan threads
+        for i in range(num_tabs_to_open):
+            thread = relevant_threads[i]
+            thread_link = thread["link"]
+
+        # Open the thread in a we browser tab
+        webbrowser.open_new_tab(thread_link)
+
+        # Used for a delay to stay within rate
+        time.sleep(1)  
+
+    # General exception handling         
+    except requests.exceptions.RequestException as err: 
+        print("Error:", err)
