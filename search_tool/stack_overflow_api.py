feecf286af811231f4594059e1c6279e95dd689f
Nathan Owens <nmowens95@gmail.com>
2024-01-30 13:04:17 -0600

    Missed adding to last commit

--- search_tool/stack_overflow_api.py
+++ search_tool/stack_overflow_api.py
@@ -2,37 +2,38 @@ import requests
 import time
 import webbrowser
 
-def search_stackoverflow(error_type, error_message, max_tabs=1):
-    # Stack exchange API base url
-    api_url = "https://api.stackexchange.com/"
+class Search():
+    def search_stackoverflow(error_type, error_message, max_tabs=1):
+        # Stack exchange API base url
+        api_url = "https://api.stackexchange.com/"
 
 
-    try:
-        # Make an API request using the relevant parameters
-        response = requests.get(f"{api_url}"+f"/2.3/search?order=desc&tagged=python&sort=activity&site=stackoverflow&q={error_message}".format(error_type))
-        response.raise_for_status() # returns HTTPError object if an error occurs during the process
+        try:
+            # Make an API request using the relevant parameters
+            response = requests.get(f"{api_url}"+f"/2.3/search?order=desc&tagged=python&sort=activity&site=stackoverflow&q={error_message}".format(error_type))
+            response.raise_for_status() # returns HTTPError object if an error occurs during the process
 
-        # Parses data
-        data = response.json()
+            # Parses data
+            data = response.json()
 
-        # Get the list of relevant threads
-        relevant_threads = data["items"]
+            # Get the list of relevant threads
+            relevant_threads = data["items"]
 
-        # Determine how many tabs to open
-        num_tabs_to_open = min(max_tabs, len(relevant_threads))
+            # Determine how many tabs to open
+            num_tabs_to_open = min(max_tabs, len(relevant_threads))
 
-        # Open relevan threads
-        for i in range(num_tabs_to_open):
-            thread = relevant_threads[i]
-            thread_link = thread["link"]
+            # Open relevan threads
+            for i in range(num_tabs_to_open):
+                thread = relevant_threads[i]
+                thread_link = thread["link"]
 
-        # Open the thread in a we browser tab
-        webbrowser.open_new_tab(thread_link)
-        print("Opened:", thread_link)
+            # Open the thread in a we browser tab
+            webbrowser.open_new_tab(thread_link)
+            print("Opened:", thread_link)
 
-        # Used for a delay to stay within rate
-        time.sleep(2)  
+            # Used for a delay to stay within rate
+            time.sleep(2)  
 
-    # General exception handling         
-    except requests.exceptions.RequestException as err: 
-        print("Error:", err)
+        # General exception handling         
+        except requests.exceptions.RequestException as err: 
+            print("Error:", err)
